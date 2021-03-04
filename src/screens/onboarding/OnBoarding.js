ebde40fe928f4037f329b4791ac1669f0ebca356
VIctory <garubav@gmail.com>
2021-03-04 17:51:45 +0100

    refactor ViewPager

--- src/screens/onboarding/OnBoarding.js
+++ src/screens/onboarding/OnBoarding.js
@@ -1,40 +1,52 @@
 import React, {useState} from 'react';
-import {Layout, ViewPager} from '@ui-kitten/components';
+import {Layout, ViewPager, StyleService} from '@ui-kitten/components';
 import Welcome from './../../components/onboarding/Welcome';
 import Info from './../../components/onboarding/Info';
+import Login from './../../components/onboarding/Login';
 import BottomNav from '../../components/onboarding/BottomNav';
 
 const OnBoarding = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-
+  const screens = [
+    {Component: Welcome, callback: null},
+    {
+      Component: Info,
+      callback: () => {
+        setSelectedIndex(0);
+      },
+    },
+    {
+      Component: Login,
+      callback: () => {
+        setSelectedIndex(1);
+      },
+    },
+  ];
   return (
     <ViewPager
       selectedIndex={selectedIndex}
       onSelect={(index) => setSelectedIndex(index)}
-      style={{flex: 1}}>
-      <Layout level="2" style={{height: '100%'}}>
-        <Welcome />
-        <BottomNav
-          route={{currentRoute: 0, routes: 4}}
-          callback={() => {
-            setSelectedIndex(1);
-          }}
-        />
-      </Layout>
-      <Layout level="2" style={{height: '100%'}}>
-        <Info
-          callback={() => {
-            setSelectedIndex(0);
-          }}
-        />
-        <BottomNav
-          route={{currentRoute: 1, routes: 4}}
-          callback={() => {
-            setSelectedIndex(2);
-          }}
-        />
-      </Layout>
+      style={styles.pager}>
+      {screens.map(({Component, callback}, i) => (
+        <Layout level="2" style={styles.layout} key={i}>
+          <Component callback={callback} />
+          <BottomNav
+            route={{currentRoute: i, routes: screens.length}}
+            callback={() => {
+              setSelectedIndex(i + 1);
+            }}
+          />
+        </Layout>
+      ))}
     </ViewPager>
   );
 };
+const styles = StyleService.create({
+  pager: {
+    flex: 1,
+  },
+  layout: {
+    height: '100%',
+  },
+});
 export default OnBoarding;
