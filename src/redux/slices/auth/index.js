f1ea56d0c39334e145269f0c69f40708005ad040
VIctory <garubav@gmail.com>
2021-03-14 22:55:18 +0100

    refactor utils functions

--- src/redux/slices/auth/index.js
+++ src/redux/slices/auth/index.js
@@ -1,10 +1,8 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {AuthThunks} from '../../thunks';
+import {AuthThunks, Utils} from '../../thunks';
 
 const {authLogin, saveToken, authSignUp} = AuthThunks;
-const loadingState = (state, payload) => {
-  state.loading = payload;
-};
+const {errorMessage, loadingState} = Utils;
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -15,22 +13,28 @@ export const authSlice = createSlice({
   extraReducers: {
     [authLogin.pending]: (state) => {
       loadingState(state, true);
+      errorMessage(state, null);
     },
     [authLogin.fulfilled]: (state, action) => {
       loadingState(state, false);
+      errorMessage(state, null);
       const {token} = action.payload;
       state.isLoggedIn = true;
       saveToken(token);
     },
     [authLogin.rejected]: (state, action) => {
       loadingState(state, false);
+      errorMessage(state, null);
       state.error = action.error.message;
     },
     [authSignUp.pending]: (state) => {
       loadingState(state, true);
+      errorMessage(state, null);
     },
     [authSignUp.fulfilled]: (state, action) => {
       loadingState(state, false);
+      errorMessage(state, null);
+
       const {token} = action.payload;
       state.isLoggedIn = true;
       saveToken(token);
