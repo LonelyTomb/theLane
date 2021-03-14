9873afc8ac1cfa2705fb86fb095c0b0844841d0a
VIctory <garubav@gmail.com>
2021-03-14 22:58:00 +0100

    create default home screens

---
+++ src/screens/home/Browse.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout, Text} from '@ui-kitten/components';
+
+const Browse = () => {
+  return (
+    <Layout>
+      <Text>Browse</Text>
+    </Layout>
+  );
+};
+
+export default Browse;
---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout, Text} from '@ui-kitten/components';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Text>Home</Text>
+    </Layout>
+  );
+};
+
+export default Home;
