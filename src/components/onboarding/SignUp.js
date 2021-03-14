f42385dc730d824c5f3483b628a7d575806b40d6
VIctory <garubav@gmail.com>
2021-03-14 22:57:29 +0100

    refactor auth form

--- src/components/onboarding/SignUp.js
+++ src/components/onboarding/SignUp.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import {
   Text,
   TopNavigation,
@@ -7,47 +7,28 @@ import {
   Layout,
   StyleService,
   Icon,
-  Input,
-  Divider,
-  Button,
   useTheme,
 } from '@ui-kitten/components';
 import {SafeAreaView, TouchableOpacity} from 'react-native';
 import {AuthThunks} from '../../redux/thunks';
-import EncryptedStorage from 'react-native-encrypted-storage';
-import {token_chars} from '@env';
+import AuthForm from './AuthForm';
 
-const SignUp = ({callback, skipCallback}) => {
+const SignUp = ({callback, skipCallback, successCallback}) => {
   const theme = useTheme();
-  const dispatch = useDispatch();
-  const {authLogin} = AuthThunks;
-  const auth = useSelector((state) => state.auth);
-  const [form, setForm] = useState({
-    email: 'test12@gmail.com',
-    password: 'password',
-  });
-  const [secureText, setSecureText] = useState(true);
-
-  console.log(auth);
+  const {authSignUp} = AuthThunks;
+  const {isLoggedIn, error} = useSelector((state) => state.auth);
 
   useEffect(() => {
-    const getToken = async () => {
-      const token = await EncryptedStorage.getItem(token_chars);
-      console.log(token);
-    };
-    getToken().then();
-  }, []);
-  const handleChange = (name, value) => {
-    setForm({...form, [name]: value});
-  };
-  const toggleSecureText = () => {
-    setSecureText(!secureText);
-  };
+    if (isLoggedIn) {
+      successCallback();
+    }
+  }, [error, isLoggedIn, successCallback]);
 
-  const toggleSecureTextIcon = (props) => (
-    <TouchableOpacity onPress={toggleSecureText}>
-      <Icon {...props} name={secureText ? 'eye-off' : 'eye'} pack={'eva'} />
-    </TouchableOpacity>
+  const BackIcon = (props) => (
+    <Icon {...props} name={'arrow-ios-back-outline'} pack={'eva'} />
+  );
+  const BackAction = () => (
+    <TopNavigationAction icon={BackIcon} onPress={callback} />
   );
 
   const styles = StyleService.create({
@@ -84,54 +65,8 @@ const SignUp = ({callback, skipCallback}) => {
       width: '60%',
       marginBottom: '10%',
     },
-    authIconsWrapper: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      paddingTop: 20,
-      paddingHorizontal: '15%',
-    },
-    icon: {
-      backgroundColor: '#FFFFFF',
-      width: 55,
-      height: 50,
-      borderRadius: 10,
-      elevation: 2,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: 10,
-    },
-    iconText: {
-      fontSize: 12,
-      color: '#c6c6c6',
-      textAlign: 'center',
-    },
-    authIcon: {
-      width: 25,
-      height: 25,
-    },
-    input: {
-      marginBottom: 5,
-    },
-    dividerLayout: {
-      marginTop: '5%',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    divider: {
-      width: '45%',
-    },
-    dividerText: {
-      color: 'grey',
-      fontSize: 14,
-    },
   });
-  const BackIcon = (props) => (
-    <Icon {...props} name={'arrow-ios-back-outline'} pack={'eva'} />
-  );
-  const BackAction = () => (
-    <TopNavigationAction icon={BackIcon} onPress={callback} />
-  );
+
   return (
     <SafeAreaView style={styles.safe}>
       <TopNavigation
@@ -156,61 +91,7 @@ const SignUp = ({callback, skipCallback}) => {
         <Text style={styles.welcomeSubText} category={'s1'}>
           Synchronize your bookmarks, history across all devices
         </Text>
-        <Layout>
-          <Input
-            value={form.email}
-            style={styles.input}
-            placeholder={'Enter your email'}
-            onChangeText={(e) => {
-              handleChange('email', e);
-            }}
-          />
-          <Input
-            value={form.password}
-            style={styles.input}
-            placeholder={'Enter password'}
-            onChangeText={(e) => {
-              handleChange('password', e);
-            }}
-            secureTextEntry={secureText}
-            accessoryRight={toggleSecureTextIcon}
-          />
-          <Button
-            onPress={async () => {
-              try {
-                const tee = await dispatch(authLogin(form));
-                // console.log('test', tee);
-              } catch (e) {
-                console.log('err', e);
-              }
-            }}>
-            Sign Up
-          </Button>
-        </Layout>
-        <Layout style={styles.dividerLayout}>
-          <Divider style={styles.divider} />
-          <Text style={styles.dividerText} category={'p1'}>
-            Or
-          </Text>
-          <Divider style={styles.divider} />
-        </Layout>
-        <Layout style={styles.authIconsWrapper}>
-          <Layout>
-            <TouchableOpacity>
-              <Layout style={styles.icon}>
-                <Icon
-                  style={styles.authIcon}
-                  name={'google'}
-                  pack={'eva'}
-                  fill={theme['color-primary-400']}
-                />
-              </Layout>
-            </TouchableOpacity>
-            <Text category={'p1'} style={styles.iconText}>
-              Gmail
-            </Text>
-          </Layout>
-        </Layout>
+        <AuthForm buttonTitle={'Sign Up'} callback={authSignUp} />
       </Layout>
     </SafeAreaView>
   );
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,53 +1,20 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {
-  Text,
-  TopNavigation,
-  TopNavigationAction,
-  Layout,
-  StyleService,
-  Icon,
-  Input,
-  Divider,
-  Button,
-  useTheme,
-} from '@ui-kitten/components';
-import {SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import {Text, Layout, StyleService, useTheme} from '@ui-kitten/components';
+import {SafeAreaView} from 'react-native';
 import {AuthThunks} from '../redux/thunks';
-import EncryptedStorage from 'react-native-encrypted-storage';
-import {token_chars} from '@env';
+import AuthForm from '../components/onboarding/AuthForm';
 
-const Login = () => {
+const Login = ({navigation}) => {
   const theme = useTheme();
-  const dispatch = useDispatch();
   const {authLogin} = AuthThunks;
-  const auth = useSelector((state) => state.auth);
-  const [form, setForm] = useState({
-    email: 'test12@gmail.com',
-    password: 'password',
-  });
-  const [secureText, setSecureText] = useState(true);
-
-  console.log(auth);
+  const {isLoggedIn} = useSelector((state) => state.auth);
 
   useEffect(() => {
-    const getToken = async () => {
-      const token = await EncryptedStorage.getItem(token_chars);
-    };
-    getToken().then();
-  }, []);
-  const handleChange = (name, value) => {
-    setForm({...form, [name]: value});
-  };
-  const toggleSecureText = () => {
-    setSecureText(!secureText);
-  };
-
-  const toggleSecureTextIcon = (props) => (
-    <TouchableOpacity onPress={toggleSecureText}>
-      <Icon {...props} name={secureText ? 'eye-off' : 'eye'} pack={'eva'} />
-    </TouchableOpacity>
-  );
+    if (isLoggedIn) {
+      navigation.navigate('Home');
+    }
+  }, [isLoggedIn, navigation]);
 
   const styles = StyleService.create({
     safe: {
@@ -66,11 +33,6 @@ const Login = () => {
       paddingHorizontal: 15,
       paddingBottom: '20%',
     },
-    skipButton: {
-      backgroundColor: 'rgba(255,255,255,0)',
-      marginRight: 15,
-      color: '#C6C6C6',
-    },
     welcomeText: {
       color: theme['color-primary-600'],
       width: '75%',
@@ -83,51 +45,7 @@ const Login = () => {
       width: '60%',
       marginBottom: '10%',
     },
-    authIconsWrapper: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      paddingTop: 20,
-      paddingHorizontal: '15%',
-    },
-    icon: {
-      backgroundColor: '#FFFFFF',
-      width: 55,
-      height: 50,
-      borderRadius: 10,
-      elevation: 2,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginBottom: 10,
-    },
-    iconText: {
-      fontSize: 12,
-      color: '#c6c6c6',
-      textAlign: 'center',
-    },
-    authIcon: {
-      width: 25,
-      height: 25,
-    },
-    input: {
-      marginBottom: 5,
-    },
-    dividerLayout: {
-      marginTop: '5%',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    divider: {
-      width: '45%',
-    },
-    dividerText: {
-      color: 'grey',
-      fontSize: 14,
-    },
   });
-  const BackIcon = (props) => (
-    <Icon {...props} name={'arrow-ios-back-outline'} pack={'eva'} />
-  );
 
   return (
     <SafeAreaView style={styles.safe}>
@@ -138,61 +56,7 @@ const Login = () => {
         <Text style={styles.welcomeSubText} category={'s1'}>
           Synchronize your bookmarks, history across all devices
         </Text>
-        <Layout>
-          <Input
-            value={form.email}
-            style={styles.input}
-            placeholder={'Enter your email'}
-            onChangeText={(e) => {
-              handleChange('email', e);
-            }}
-          />
-          <Input
-            value={form.password}
-            style={styles.input}
-            placeholder={'Enter password'}
-            onChangeText={(e) => {
-              handleChange('password', e);
-            }}
-            secureTextEntry={secureText}
-            accessoryRight={toggleSecureTextIcon}
-          />
-          <Button
-            onPress={async () => {
-              try {
-                const tee = await dispatch(authLogin(form));
-                // console.log('test', tee);
-              } catch (e) {
-                console.log('err', e);
-              }
-            }}>
-            Login
-          </Button>
-        </Layout>
-        <Layout style={styles.dividerLayout}>
-          <Divider style={styles.divider} />
-          <Text style={styles.dividerText} category={'p1'}>
-            Or
-          </Text>
-          <Divider style={styles.divider} />
-        </Layout>
-        <Layout style={styles.authIconsWrapper}>
-          <Layout>
-            <TouchableOpacity>
-              <Layout style={styles.icon}>
-                <Icon
-                  style={styles.authIcon}
-                  name={'google'}
-                  pack={'eva'}
-                  fill={theme['color-primary-400']}
-                />
-              </Layout>
-            </TouchableOpacity>
-            <Text category={'p1'} style={styles.iconText}>
-              Gmail
-            </Text>
-          </Layout>
-        </Layout>
+        <AuthForm buttonTitle={'Log In'} callback={authLogin} />
       </Layout>
     </SafeAreaView>
   );
