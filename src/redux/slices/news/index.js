4042ed5aec819ef67f6483fe6b83943dff99c9e5
VIctory <garubav@gmail.com>
2021-03-19 13:56:06 +0100

    create NewsSlice

---
+++ src/redux/slices/news/index.js
@@ -0,0 +1,30 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {NewsThunks, Utils} from '../../thunks';
+
+const {topHeadlines} = NewsThunks;
+const {errorMessage, loadingState} = Utils;
+
+export const newsSlice = createSlice({
+  name: 'user',
+  initialState: {loading: false, headlines: {}},
+  reducers: {},
+  extraReducers: {
+    [topHeadlines.pending]: (state) => {
+      loadingState(state, true);
+      errorMessage(state, null);
+    },
+    [topHeadlines.fulfilled]: (state, action) => {
+      loadingState(state, false);
+      errorMessage(state, null);
+      console.log(action.payload);
+    },
+    [topHeadlines.rejected]: (state, action) => {
+      loadingState(state, false);
+      errorMessage(state, null);
+    },
+  },
+});
+
+export const {} = newsSlice.actions;
+
+export default newsSlice.reducer;
