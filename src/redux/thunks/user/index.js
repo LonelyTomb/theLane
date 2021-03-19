021da977757320e7907d7cdf00e96d5d5ad02e14
VIctory <garubav@gmail.com>
2021-03-19 13:53:39 +0100

    add try catch block

--- src/redux/thunks/user/index.js
+++ src/redux/thunks/user/index.js
@@ -1,10 +1,17 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {UserAPI} from '../../../api';
 
-const getUser = createAsyncThunk('user/getUser', async (payload, thunkAPI) => {
-  const response = await UserAPI.get('/');
-  return response.data;
-});
+const getUser = createAsyncThunk(
+  'user/getUser',
+  async (payload, {rejectWithValue}) => {
+    try {
+      const response = await UserAPI.get('/');
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  },
+);
 
 const UserThunks = {getUser};
 
