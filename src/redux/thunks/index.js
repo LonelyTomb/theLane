babd848dce908b81db1268394b002f72d4907408
VIctory <garubav@gmail.com>
2021-03-19 13:55:26 +0100

    add NewsThunk

--- src/redux/thunks/index.js
+++ src/redux/thunks/index.js
@@ -1,5 +1,6 @@
 import AuthThunks from './auth';
 import UserThunks from './user';
+import NewsThunks from './news';
 import Utils from './utils';
 
-export {AuthThunks, UserThunks, Utils};
+export {AuthThunks, UserThunks, NewsThunks, Utils};
