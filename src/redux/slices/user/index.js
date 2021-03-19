c583d326682a3af53cd29cf678c1abb44951c6ac
VIctory <garubav@gmail.com>
2021-03-19 13:54:08 +0100

    optimize functions

--- src/redux/slices/user/index.js
+++ src/redux/slices/user/index.js
@@ -1,27 +1,27 @@
 import {createSlice} from '@reduxjs/toolkit';
-import {UserThunks} from '../../thunks';
+import {UserThunks, Utils} from '../../thunks';
 
 const {getUser} = UserThunks;
-const loadingState = (state, payload) => {
-  state.loading = payload;
-};
+const {errorMessage, loadingState} = Utils;
 
 export const userSlice = createSlice({
   name: 'user',
-  initialState: {loading: false, user: {}},
+  initialState: {loading: false, user: {}, error: null},
   reducers: {},
   extraReducers: {
     [getUser.pending]: (state) => {
       loadingState(state, true);
+      errorMessage(state, null);
     },
     [getUser.fulfilled]: (state, action) => {
       loadingState(state, false);
+      errorMessage(state, null);
       const {user} = action.payload;
       state.user = {...user};
     },
     [getUser.rejected]: (state, action) => {
       loadingState(state, false);
-      state.error = action.error.message;
+      errorMessage(state, action.error.message);
     },
   },
 });
