b56684bdb4680c568e5a3176ab4ec9509e9d8ae0
VIctory <garubav@gmail.com>
2021-03-19 13:57:56 +0100

    add newsReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import userReducer from './../slices/user';
 import authReducer from './../slices/auth';
+import newsReducer from './../slices/news';
 
 const appReducers = combineReducers({
   users: userReducer,
   auth: authReducer,
+  news: newsReducer,
 });
 const rootReducers = (state, action) => {
   if (action.type === 'RESET_APP') {
